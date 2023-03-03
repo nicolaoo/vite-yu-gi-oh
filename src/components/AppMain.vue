f91286d4518d383e671571483d9b4b1f7bda6e93
nicola00 <nicolajobs00@gmail.com>
2023-03-03 17:22:20 +0100

    FEAT axios and bring the card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,29 @@
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
-
+            card: []
         }
+    },
+    computed: {
+        dataCard() {
+            return res.data.data
+        }
+    },
+    methods: {
+        getCard() {
+            axios.
+                get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                .then((res) => {
+                    console.log(res.data.data)
+
+                })
+        }
+    },
+    created() {
+        this.getCard()
     }
 }
 
@@ -19,78 +39,6 @@ export default {
                     </h4>
                 </div>
                 <div class="grid">
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure>
-                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
-                        </figure>
-                        <div class="feature">
-                            <h5>'A' cell breending device </h5>
-                            <p>Alien</p>
-                        </div>
-                    </div>
                     <div class="card">
                         <figure>
                             <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
