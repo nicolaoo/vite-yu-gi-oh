ded0eaf7eceb7c857589866c802e8a1593ca6d8d
nicola00 <nicolajobs00@gmail.com>
2023-03-03 17:11:55 +0100

    FEAT layout card static and style

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,6 +9,148 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="head-grid">
+                    <h4>
+                        Found 39 Cards
+                    </h4>
+                </div>
+                <div class="grid">
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <figure>
+                            <img src="https://static-it.gamestop.it/images/products/301603/3max.jpg" alt="">
+                        </figure>
+                        <div class="feature">
+                            <h5>'A' cell breending device </h5>
+                            <p>Alien</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
  
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+    background-color: darkorange;
+}
+
+.card {
+    padding: 5px;
+
+    .feature {
+        background-color: white;
+        padding: 10px;
+
+        h5 {
+            text-align: center;
+            color: darkorange;
+            text-transform: uppercase;
+            font-size: 10px;
+        }
+
+        p {
+            text-align: center;
+            padding-top: 12px;
+        }
+    }
+}
+
+.grid {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+}
+
+.row {
+    padding: 20px;
+
+    .head-grid {
+        background-color: rgb(19, 19, 26);
+        padding: 10px 0;
+
+        h4 {
+            padding-left: 10px;
+            color: white;
+            text-transform: uppercase;
+            font-size: 12px;
+            line-height: 15px;
+        }
+    }
+}
+</style>
